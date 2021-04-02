f1c4b8328590ae0a50d5483737fe9f496553dbea
André Lima <andredlima23@gmail.com>
2021-04-02 00:28:24 -0300

    criação da conexão ao banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
-
-    response.json([
-        'Nícolas',
-        'Ísis',
-        'André',
-        'Joyce'
-    ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);

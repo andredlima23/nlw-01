ab01b55f3df13579da2a86120c136fa4133b0b5e
André Lima <andredlima23@gmail.com>
2021-04-06 00:10:29 -0300

    Aula 2 NLW01

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 
+import PointController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = express.Router();
+const pointController = new PointController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
+routes.get('/items', itemsController.index);
 
-    return response.json({ message: 'Hello world' });
-});
+routes.post('/points', pointController.create);
+routes.get('/points/', pointController.index);
+routes.get('/points/:id', pointController.show);
 
 export default routes;
